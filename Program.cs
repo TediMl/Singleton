9bdc08123888c8f80ba9482961471acb7e489bc8
TediMl <114427584+TediMl@users.noreply.github.com>
2024-03-11 12:05:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Singleton;
+
+public class Program
+{
+    public static void Main()
+    {
+        var numbers = Enumerable.Range(0, 10);
+        foreach (var i in numbers)
+        {
+            var vm = VoteMachine.Instance;
+            vm.RegisterVote();
+        }
+        Console.WriteLine(VoteMachine.Instance.TotalVote);
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ VoteMachine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class VoteMachine
+    {
+        private static VoteMachine _instance = null;
+
+        private int _totalVote;
+        private static readonly object lockOb = new object();
+
+        private VoteMachine()
+        {
+        }
+        public static VoteMachine Instance //access the single instance of the class
+        {
+            get
+            {
+                if (_instance == null) //thread-safe Sing
+                {
+                    lock (lockOb)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new VoteMachine();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void RegisterVote()
+        {
+            lock (lockOb)
+            {
+                _totalVote += 1;
+                Console.WriteLine("Registered Vote #" + _totalVote);
+
+            }
+
+        }
+
+        public int TotalVote
+        {
+
+            get
+            {
+                lock (lockOb)
+                {
+                    return _totalVote;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
